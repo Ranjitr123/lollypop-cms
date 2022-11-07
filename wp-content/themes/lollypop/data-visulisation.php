146dab260c7c6bd01e38322cfb1b59a506c948f6
Swatikumari49 <102963630+Swatikumari49@users.noreply.github.com>
2022-11-07 17:10:18 +0530

    updated data  visualisation.php

--- wp-content/themes/lollypop/data-visulisation.php
+++ wp-content/themes/lollypop/data-visulisation.php
@@ -17,9 +17,9 @@ get_header();
           <div class="row justify-content-center">
             <div class="col-12 col-md-11 ">
               <div class="page-head">
-                <h1 class="fnt-800 px-md-0 mb-0 text-center">Data Visualisation to</h1>
-                <h1 class="fnt-800 px-md-0 mb-0 text-center mw-682"><span class="data_red1">Reveal information within your data</span></h1>
-                <p class="fnt-24 text-center mw-634">Become a storyteller by turning your data into visually compelling information. </p>
+                <h1 class="fnt-800 px-md-0 mb-0 text-center"><?php echo get_field('title1')?></h1>
+                <h1 class="fnt-800 px-md-0 mb-0 text-center mw-682"><span class="data_red1"><?php echo get_field('title_red')?></span></h1>
+                <p class="fnt-24 text-center mw-634"><?php echo get_field('description')?> </p>
               </div>
             </div>
           </div>
@@ -33,9 +33,9 @@ get_header();
           <div class='col-12 '>
             <div class="play-video-on-scroll">
               <div id="play2-out" style="display:none">
-                <div id="play2" data-plyr-provider="vimeo" data-plyr-embed-id="https://player.vimeo.com/video/745332440"></div>
+                <div id="play2" data-plyr-provider="vimeo" data-plyr-embed-id="<?php echo get_field('video_link')?>"></div>
               </div>
-              <video class="playvid" autoplay="" muted="" loop="" playsinline="" provider=" vimeo" embed-id="https://player.vimeo.com/video/745332440" poster="http://localhost/cms/lollypop-cms/wp-content/uploads/2022/11/Mask-Group.png">
+              <video class="playvid" autoplay="" muted="" loop="" playsinline="" provider=" vimeo" embed-id="<?php echo get_field('video_link')?>" poster="<?php echo get_field('image')?>">
               </video>
             </div>
           </div>
@@ -48,52 +48,27 @@ get_header();
     <div class="container">
         <div class="row">
             <div class="col-12 col-md-10 col-lg-7 mx-auto">
-                <p class="firstp">Some of the stories we helped transform</p>
+                <p class="firstp"><?php echo get_field('titles')?></p>
                 <div class="row">
+					<?php while(have_rows('social_icons_section')){the_row();?>
                     <div class="col-md-3 col-6">
-                        <img src="http://localhost/cms/lollypop-cms/wp-content/uploads/2022/11/Group_144_1_.svg" class="img-fluid" alt="...">
+                        <img src="<?php echo get_sub_field('image')?>" class="img-fluid" alt="...">
                     </div>
-                    <div class="col-md-3 col-6">
-                        <img src="http://localhost/cms/lollypop-cms/wp-content/uploads/2022/11/Group_145_1_.svg" class="img-fluid" alt="...">
-                    </div>
-                    <div class="col-md-3 col-6">
-                        <img src="http://localhost/cms/lollypop-cms/wp-content/uploads/2022/11/Group_144_1_.svg" class="img-fluid" alt="...">
-                    </div>
-                    <div class="col-md-3 col-6">
-                        <img src="http://localhost/cms/lollypop-cms/wp-content/uploads/2022/11/Group_145_1_.svg" class="img-fluid" alt="...">
-                    </div>
+					<?php } ?>   
+
                 </div>
             </div>
         </div>
 
         <div class="row">
             <div class="col-12 col-md-10 col-lg-7 mx-auto">
-                <p class="firstp pt-5">Some of the stories we helped transform</p>
+                <p class="firstp pt-5"><?php echo get_field('client_title')?></p>
                 <div class="row">
+					<?php while(have_rows('client_image_section')){the_row();?>
                     <div class="col-md-3 col-6">
-                        <img src="http://localhost/cms/lollypop-cms/wp-content/uploads/2022/11/Group_144_1_.svg" class="img-fluid" alt="...">
+                        <img src="<?php echo get_sub_field('image')?>" class="img-fluid" alt="...">
                     </div>
-                    <div class="col-md-3 col-6">
-                        <img src="http://localhost/cms/lollypop-cms/wp-content/uploads/2022/11/Group_145_1_.svg" class="img-fluid" alt="...">
-                    </div>
-                    <div class="col-md-3 col-6">
-                        <img src="http://localhost/cms/lollypop-cms/wp-content/uploads/2022/11/Group_144_1_.svg" class="img-fluid" alt="...">
-                    </div>
-                    <div class="col-md-3 col-6">
-                        <img src="http://localhost/cms/lollypop-cms/wp-content/uploads/2022/11/Group_145_1_.svg" class="img-fluid" alt="...">
-                    </div>
-                    <div class="col-md-3 col-6">
-                        <img src="http://localhost/cms/lollypop-cms/wp-content/uploads/2022/11/Group_144_1_.svg" class="img-fluid" alt="...">
-                    </div>
-                    <div class="col-md-3 col-6">
-                        <img src="http://localhost/cms/lollypop-cms/wp-content/uploads/2022/11/Group_145_1_.svg" class="img-fluid" alt="...">
-                    </div>
-                    <div class="col-md-3 col-6">
-                        <img src="http://localhost/cms/lollypop-cms/wp-content/uploads/2022/11/Group_144_1_.svg" class="img-fluid" alt="...">
-                    </div>
-                    <div class="col-md-3 col-6">
-                        <img src="http://localhost/cms/lollypop-cms/wp-content/uploads/2022/11/Group_145_1_.svg" class="img-fluid" alt="...">
-                    </div>
+					<?php } ?>  
                 </div>
             </div>
         </div>
@@ -101,50 +76,28 @@ get_header();
         <div class="need_data">
             <div class="row">
                 <div class="col-12 col-lg-8 mx-auto">
-                    <h2>Why do we need Data Visualisation?</h2>
-                    <p>In the world of Big Data, data visualization is essential to analyze massive amounts of information and make meaningful decisions that are data driven.</p>
+                    <h2><?php echo get_field('heading')?></h2>
+                    <p><?php echo get_field('content')?></p>
                 </div>
             </div>
                 <div class="row">
                     <div class=" col-lg-8 mx-auto">
                         <div class="row">
+							<?php while(have_rows('content_section')){the_row();?>
                             <div class="col-md-6 space">
-                                <p class="heading">Fast Conception of Information</p>
-                                <p class="para">Clearly communicate huge and complex data, including big data effectively & efficiently. Focus on other future success indicators by saving time here.</p>
+                                <p class="heading"><?php echo get_sub_field('title')?></p>
+                                <p class="para"><?php echo get_sub_field('content')?></p>
                             </div>
-
-                            <div class="col-md-6">
-                                <p class="heading">Discover & Predict New Trends</p>
-                                <p class="para">Discover new emerging trends in the market before the barriers stop decisive opportunities for growth and remove the noise from your data.</p>
-                            </div>
-
-                            <div class="col-md-6 space">
-                                <p class="heading">Find out Relationships and Patterns</p>
-                                <p class="para">Present your data graphically to understand different relationships and identify various patterns to point out the pain points affecting your business.</p>
-                            </div>
-
-                            <div class="col-md-6">
-                                <p class="heading">Present your Data</p>
-                                <p class="para">Showcase your data in visually appealing, impactful and highly insightful ways to the stakeholders. </p>
-                            </div>
-
-                            <div class="col-md-6 space">
-                                <p class="heading">Smart Decision Making</p>
-                                <p class="para">Enable faster and smarter decision making based on facts and discoveries.</p>
-                            </div>
-                            <p class="para ps-3"><span class="data_red">Your Data, Our Insights </span>for all you data visualisation needs <span class="data_red1 position-relative">Get in Touch</span></p>
-                        </div>
-                    
-                        
-                    
+							<?php } ?>  
+                            <p class="para ps-3">
+							<?php while(have_rows('bottom_section')){the_row();?>
+								<span class="data_red"><?php echo get_sub_field('red_text1')?> </span><?php echo get_sub_field('black_text')?> <span class="data_red1 position-relative"><?php echo get_sub_field('red_text')?></span>
+							<?php } ?> 	
+							</p>
+                        </div>                                         
                     </div>
                 </div>
-
-
-
-        </div>
-
-        
+        </div>      
 
     </div>
 
@@ -154,46 +107,20 @@ get_header();
                     <div class=" col-lg-10 mx-auto">
                         <div class="row black_row">
                             <div class="col-md-6">
-                                <h2 class="white_text1">What are the Use Cases for Data Visulisation</h2>
-                                <p class="white_text2">Data visualization in one of the processes in Data science pipeline, that enables the information to be represented in a visually compelling, easy to understand format.
+								<?php while(have_rows('left_section')){the_row();?>
+                                <h2 class="white_text1"><?php echo get_sub_field('heading')?></h2>
+                                <p class="white_text2"><?php echo get_sub_field('description')?></p>
+								<?php } ?> 	
                             </div>
 
                             <div class="col-md-6">
+								<?php while(have_rows('right_section')){the_row();?>
                                 <div class="pb-5">
-                                    <img src="http://localhost/cms/lollypop-cms/wp-content/uploads/2022/11/Slot.svg" class="img-fluid pb-2 m-0">
-                                    <p class="white_text3 pb-2">Data Science</p>
-                                    <p class="white_text2 mw-362">Helps in complicated data analysis, to better comprehend data sets, spot patterns and seamlessly communicate information.</p>
+                                    <img src="<?php echo get_sub_field('icon')?>" class="img-fluid pb-2 m-0">
+                                    <p class="white_text3 pb-2"><?php echo get_sub_field('title')?></p>
+                                    <p class="white_text2 mw-362"><?php echo get_sub_field('content')?></p>
                                 </div>
-
-                                <div class="pb-5">
-                                    <img src="http://localhost/cms/lollypop-cms/wp-content/uploads/2022/11/Slot.svg" class="img-fluid pb-2 m-0">
-                                    <p class="white_text3 pb-2">E-Commerce</p>
-                                    <p class="white_text2 mw-362">Identifies consumer trends, gives rapid insights to attract new customers and in turn improve customer experience.</p>
-                                </div>
-
-                                <div class="pb-5">
-                                    <img src="http://localhost/cms/lollypop-cms/wp-content/uploads/2022/11/Slot.svg" class="img-fluid pb-2 m-0">
-                                    <p class="white_text3 pb-2">Financials</p>
-                                    <p class="white_text2 mw-362">Emphasizes recent trends to devise better investment strategies, resulting in improved business prospects.  Drill down into various data aspects to propel the business in the right direction.</p>
-                                </div>
-
-                                <div class="pb-5">
-                                    <img src="http://localhost/cms/lollypop-cms/wp-content/uploads/2022/11/Slot.svg" class="img-fluid pb-2 m-0">
-                                    <p class="white_text3 pb-2">Education</p>
-                                    <p class="white_text2 mw-362">Allows inexperienced users to analyze data and make meaningful decisions. Provides intuitive and interactive dashboards that makes monitoring student progress possible.</p>
-                                </div>
-
-                                <div class="pb-5">
-                                    <img src="http://localhost/cms/lollypop-cms/wp-content/uploads/2022/11/Slot.svg" class="img-fluid pb-2 m-0">
-                                    <p class="white_text3 pb-2">Business intelligence</p>
-                                    <p class="white_text2 mw-362">Standardized display of data and make it easier for the user to see bigger volumes of data, and provide a cohesive view of an organization’s entire data system.</p>
-                                </div>
-
-                                <div class="pb-5">
-                                    <img src="http://localhost/cms/lollypop-cms/wp-content/uploads/2022/11/Slot.svg" class="img-fluid pb-2 m-0">
-                                    <p class="white_text3 pb-2">Artificial Intelligence & Machine Learning</p>
-                                    <p class="white_text2 mw-362">Analyze historical data to provide insights into business-critical incidents.</p>
-                                </div>
+								<?php } ?>
                             </div>
                         </div>
                     </div>
@@ -206,32 +133,19 @@ get_header();
         <div class="need_data need_data1">
             <div class="row">
                 <div class="col-12 col-lg-8 mx-auto">
-                <h2>What services do we provide?</h2>
+                <h2><?php echo get_field('service_heading')?></h2>
                 </div>
             </div>
 
             <div class="row">
                 <div class=" col-lg-8 mx-auto">
                     <div class="row">
+						<?php while(have_rows('service_content_section')){the_row();?>
                         <div class="col-md-6 space">
-                            <p class="heading">Dashboard optimisation</p>
-                            <p class="para">Analytical dashboards tell a more complete story than simply displaying raw data, and are limitless in the breadth of data they can communicate in a single visual.Transforming data into information that is easily consumable and immediately actionable.</p>
+                            <p class="heading"><?php echo get_sub_field('service_title')?></p>
+                            <p class="para"><?php echo get_sub_field('service_content')?></p>
                         </div>
-
-                        <div class="col-md-6">
-                            <p class="heading">Tableau to Power BI Migration</p>
-                            <p class="para">Making data analytics accessible to everyone by delivering a low-code to no-code experience for all aspects of data analysis. Visually showcase complex datasets, identify relations and trends.</p>
-                        </div>
-
-                        <div class="col-md-6 space">
-                            <p class="heading">Report Development</p>
-                            <p class="para">Report development for all the teams, from analytics to the executive to the customer.</p>
-                        </div>
-
-                        <div class="col-md-6">
-                            <p class="heading">Power BI Services</p>
-                            <p class="para">Unify your disparate sources of data into coherent, visually immersive, and interactive insights.</p>
-                        </div>
+						<?php } ?>
                         <p class="para ps-3"><span class="data_red1 position-relative">Get in Touch</span></p>
                     </div>
                     
@@ -247,21 +161,23 @@ get_header();
                     <div class="row">
                         <div class="col-12 col-md-10 col-lg-9 mx-auto">
                             <p>FAQ</p>
-                            <h2>When in doubt, always ask</h2>
+                            <h2><?php echo get_field('main_title')?></h2>
                             <div class="accordion" id="accordionExample">
-                                <div class="accordion-item">
-                                    <h3 class="accordion-header" id="headingOne">
-                                    <button class="accordion-button position-relative plus" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                                        Why do I need Data visualization?
+                                <?php $count = 0; while(have_rows('faq_section')){the_row();?>
+								<div class="accordion-item">
+                                    <h3 class="accordion-header" id="heading<?php echo $count; ?>">
+                                    <button class="accordion-button position-relative plus" type="button" data-bs-toggle="collapse" data-bs-target="#collapse<?php echo $count; ?>" aria-expanded="true" aria-controls="collapse<?php echo $count; ?>">
+                                        <?php echo get_sub_field('question')?>
                                     </button>
                                     </h3>
-                                    <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                                    <div id="collapse<?php echo $count; ?>" class="accordion-collapse collapse <?php if($count == 0){echo 'show';}?>" aria-labelledby="heading<?php echo $count; ?>" data-bs-parent="#accordionExample">
                                     <div class="accordion-body">
-                                        <p>Data visualization helps in faster conceptualisation and discovering new trends, which facilitates better understanding and decision making.</p>
+                                        <p><?php echo get_sub_field('answer')?></p>
                                     </div>
                                     </div>
                                 </div>
-                                <div class="accordion-item">
+								<?php $count++; } ?>
+                                <!--<div class="accordion-item">
                                     <h3 class="accordion-header" id="headingTwo">
                                     <button class="accordion-button collapsed plus position-relative" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
                                     How does a data visualization expert at lollypop approach a project?
@@ -296,7 +212,7 @@ get_header();
                                     </div>
                                     </div>
                                 </div>
-                            </div>
+                            </div>-->
                         </div>
                     </div>
                 </div>
