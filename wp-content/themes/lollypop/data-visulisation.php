5fc953befa4670061beb5b7dca4fd957c53e9570
Swatikumari49 <102963630+Swatikumari49@users.noreply.github.com>
2022-11-08 17:43:57 +0530

    updated dat visualisation.php

--- wp-content/themes/lollypop/data-visulisation.php
+++ wp-content/themes/lollypop/data-visulisation.php
@@ -75,13 +75,13 @@ get_header();
 
         <div class="need_data">
             <div class="row">
-                <div class="col-12 col-lg-8 mx-auto">
+                <div class="col-12 col-lg-9 mx-auto">
                     <h2><?php echo get_field('heading')?></h2>
                     <p><?php echo get_field('content')?></p>
                 </div>
             </div>
                 <div class="row">
-                    <div class=" col-lg-8 mx-auto">
+                    <div class=" col-lg-9 mx-auto">
                         <div class="row">
 							<?php while(have_rows('content_section')){the_row();?>
                             <div class="col-md-6">
