edd4bd42e6f90bd70bb38d09d8bf673ceebed1f6
Swatikumari49 <102963630+Swatikumari49@users.noreply.github.com>
2022-11-11 12:01:59 +0530

    updated aha page footer.php

--- wp-content/themes/lollypop/aha.php
+++ wp-content/themes/lollypop/aha.php
@@ -674,75 +674,7 @@ get_header();
     </div>
   </section>
 </main>
-<footer class="footer">
-  <div class="container">
-    <div class="row">
-      <div class="col-12 col-md-11 col-lg-10 mx-auto">
-        <div class="row mb-60">
-          <h5 class="fnt-16 fnt-600 mb-22 text-center">Follow our work</h5>
-          <ul class="px-0 ft-social-links d-flex flex-wrap justify-content-center">
-            <li class="ft-social-links__item col-6 col-sm-3 col-md-3 mb-3 mb-sm-0 text-center">
-              <a class="fnt-30 fnt-800 link-linkedin" href="https://www.linkedin.com/company/lollypop-studio/" target="_blank">LinkedIn</a>
-            </li>
-            <li class="ft-social-links__item col-6 col-sm-3 col-md-3 mb-3 mb-sm-0 text-center"> <a class="fnt-30 fnt-800 link-insta" href="https://www.instagram.com/lollypop.design/" targ et="_blank">Instagram</a></li>
-            <li class="ft-social-links__item col-6 col-sm-3 col-md-3 mb-3 mb-sm-0 text-center"> <a class="fnt-30 fnt-800 link-fb" href="https://www.facebook.com/lollypop.india" target="_b
-lank">Facebook</a></li>
-            <li class="ft-social-links__item col-6 col-sm-3 col-md-3 mb-3 mb-sm-0 text-center"> <a class="fnt-30 fnt-800 link-twt" href="https://twitter.com/lollypop_studio" target="_blan
-k">Twitter</a></li>
-          </ul>
-        </div>
-        <div class="row">
-          <div class="col-12 col-md-4 mb-4 mb-md-0 order-2 order-md-1 text-center text-md-left"><a class="clr-second d-inline-block fnt-24 lp-link--red fnt-800 mb-4 mb-md-2 col-8 col-lg-6
- px-0" href="<?php echo site_url(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/img/lollypop_logo1.svg" style="width:100%" alt="Image"></a>
-            <p class="fnt-14 fnt-400 clr-primary mb-0 pb-1 mb-md-3 mb-lg-4 pb-lg-0 px-0 mt-2">A&nbsp;<a href="https://www.terralogic.com/" class="fnt-800 hvr-link fnt-16 d-inline-block">T
-                erralogic</a>&nbsp;company crafting meaningful experiences through Research, Design and Development.</p>
-            <ul class="mt-5 mt-md-0 footer-logo-links fnt-400 fnt-14 d-flex justify-content-center justify-content-md-start flex-wrap px-0 mb-0">
-              <li class="footer-logo-links__item col-12 px-0">
-                <div class="fnt-12"> 2022 Lollypop - A Terralogic company</div>
-              </li>
-              <li class="footer-logo-links__item"> <a class="fnt-12" href="<?php echo site_url(); ?>/frequently-ask-questions/">FAQs</a></li>
-              <li class="footer-logo-links__item"> <a class="fnt-12" href="<?php echo site_url(); ?>/privacy-policy/">Privacy Policy</a></li>
-              <li class="footer-logo-links__item"> <a class="fnt-12" href="<?php echo site_url(); ?>/terms-of-use/">Terms of Use</a></li>
-            </ul>
-          </div>
-          <div class="col-12 col-md-7 col-lg-8 order-1 order-md-2 mb-5 mb-md-0">
-            <div class="footer-nav-links d-flex flex-wrap ps-lg-5 justify-content-center justify-content-md-left">
-              <div class="footer-nav-links__item mt-2 pt-1 pt-md-0 mt-md-0 d-flex flex-column align-items-center col-3 col-md-3">
-                <a class="d-block hvr-link-div text-center text-md-left" href="<?php echo site_url(); ?>/ui-ux-design/">
-                  <h6 class="fnt-16 fnt-800 mb-0 text-capitalize">India</h6>
-                  <div><span class="fnt-14 d-inline-block fnt-400 text-capitalize">Bangalore</span></div>
-                  <div><span class="fnt-14 d-inline-block fnt-400 text-capitalize">Mumbai</span></div>
-                  <div><span class="fnt-14 d-inline-block fnt-400 text-capitalize">Chennai</span></div>
-                  <div><span class="fnt-14 d-inline-block fnt-400 text-capitalize">Hyderabad</span></div>
-                </a>
-              </div>
-              <div class="footer-nav-links__item mt-2 pt-1 pt-md-0 mt-md-0 d-flex flex-column align-items-center col-3 col-md-3">
-                <a class="d-block hvr-link-div text-center text-md-left" href="<?php echo site_url(); ?>/ui-ux-design-agency-in-usa/">
-                  <h6 class="fnt-16 fnt-800 mb-0 text-capitalize">USA</h6>
-                  <div><span class="fnt-14 d-inline-block fnt-400 text-capitalize">San Jose</span></div>
-                  <div><span class="fnt-14 d-inline-block fnt-400 text-capitalize">Dallas</span></div>
-                  <div><span class="fnt-14 d-inline-block fnt-400 text-capitalize">Salt Lake City</span></div>
-                </a>
-              </div>
-              <div class="footer-nav-links__item mt-2 pt-1 pt-md-0 mt-md-0 d-flex flex-column align-items-center col-3 col-md-3">
-                <a class="d-block hvr-link-div text-center text-md-left" href="<?php echo site_url(); ?>/ae/">
-                  <h6 class="fnt-16 fnt-800 mb-0 text-capitalize">UAE</h6>
-                  <div><span class="fnt-14 d-inline-block fnt-400 text-capitalize">Dubai</span></div>
-                </a>
-              </div>
-              <div class="footer-nav-links__item mt-2 pt-1 pt-md-0 mt-md-0 d-flex flex-column align-items-center col-3 col-md-3">
-                <a class="d-block hvr-link-div text-center text-md-left" href="<?php echo site_url(); ?>/ui-ux-design-agency-vietnam/">
-                  <h6 class="fnt-16 fnt-800 mb-0 text-capitalize">Vietnam</h6>
-                  <div><span class="fnt-14 d-inline-block fnt-400 text-capitalize">Ho Chi Minh City</span></div>
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</footer>
+
 <div class="js-page-scroll-top"><img src="<?php echo get_template_directory_uri(); ?>/img/pg-scroll.svg" alt="Image"></div>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.js"></script>
 <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
