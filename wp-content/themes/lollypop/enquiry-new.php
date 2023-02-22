33432f49cf8981c859f0f15cd175b19939210463
Swatikumari49 <102963630+Swatikumari49@users.noreply.github.com>
2023-02-22 18:05:19 +0530

    Update enquiry-new.php

--- wp-content/themes/lollypop/enquiry-new.php
+++ wp-content/themes/lollypop/enquiry-new.php
@@ -203,7 +203,7 @@ color: red;
                         <div class="enquiry-form__item data-scroll">
                           <div class="col-md-6 col-lg-8 px-0 mb-2">
                             <h3 class="fnt-28 fnt-800 mb-1">
-                              Tell us your project requirement
+                              Tell us about your project's requirements.
                             </h3>
                             <p class="fnt-14 label">
                               Looking for UX/UI
