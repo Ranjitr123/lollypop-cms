fca0e3b67196d3fbf50cbdc89f5405c1f7fae71d
vandana <vandana.c@terralogic.com>
2023-03-03 19:35:18 +0530

    dropdown changes 2

---
+++ wp-content/themes/lollypop/trading-leagues-portifile.php
@@ -0,0 +1,167 @@
+<?php
+/**
+ Template Name: Trading League portfolio
+ */
+
+get_header();
+?>
+
+<style>
+    @media screen and (min-width: 1200px) { 
+        .portfolio_thubmnail{width: 948px;}
+
+        .sec-bnr  h1 {
+       font-size:50px !important;
+       line-height:65px !important;
+    }
+    .mrg-btm-130{
+       margin-top:130px;
+    }
+    .iphonepro-con-1{
+      margin-top:-50%;
+    }
+     .iphonepro-con-3{
+        margin-top:35%;
+     }
+     .iphonepro-con-4{
+        margin-top:-10%;
+     }
+
+
+    }
+   
+</style>
+
+<main>
+   <!-- 01 -->
+   <section class="sec-pd sec-bnr p-r-80"><div class="container"><div class="row"><div class="col-12 col-md-11 col-lg-10 mx-auto"><div class="row"><div class="col-12 col-md-11 offset-md-1"><div class="mb-r-80 col-md-10 col-lg-10 px-0"> <span class="d-block fnt-14 text-capitalize mb-2 clr-default">projects</span><h1 class="fnt-50 fnt-800 mb-3 mb-md-4 red-stroke red-stroke--small13 px-md-0">First-of-its-kind fantasy trading platform, a gamified experience of the financial markets.</h1><p class="fnt-24">A disruptive concept for an immersive experience of the financial markets making trading accessible and seamless for everyday user.</p><div class="project-type">
+<span class="fnt-14 clr-black354">Trading Leagues</span>
+<span class="fnt-14 clr-black354">India</span>
+<span class="fnt-14 clr-black354">Mobile Application Design </span></div></div></div></div><div class="reveal-project"><div class="play-video-on-scroll"><div id="play2-out" style="display:none"><div id="play2" data-plyr-provider="vimeo" data-plyr-embed-id="https://player.vimeo.com/video/764113737?h=b5566c2bf1"></div></div><video class="playvid" autoplay="" muted="" loop="" playsinline="" embed-id="https://player.vimeo.com/video/764113737?h=b5566c2bf1" provider="vimeo" poster="https://lollypop.design/wp-content/uploads/2022/10/eshakthi-project-video-thumbnail.webp"></video></div></div></div></div></div>
+</section>
+<!-- 02 -->
+<section class="p-r-80 pb-0 pare-define">
+  <div class="container">
+    <div class="row">
+      <div class="col-12 col-md-11 col-lg-10 mx-auto">
+          <div class="col-md-10 px-0 mx-auto">
+          <div class="mb-r-80">
+            <div class="project-step">
+              <div class="project-step__item row">
+                <div class="col-12 col-md-4 mb-3 mb-md-0"> 
+                  <span class="clr-second fnt-18 d-inline-block fnt-700 text-uppercase data-scroll disc-head text-rpd text-rpd text-rpd--more" style="opacity: 1; transform: translateY(0px);">Brief</span>
+                </div>
+                <div class="col-12 col-md-8 project-step-disc">
+                  <div class="project-step-disc__item">
+                    <p>TradingLeagues started with a mission to empower all users who still find hassle in stock trading. However, with a diverse mix of experienced investors, day traders, and also inexperienced users, the level of engagement per user has become complicated. Aiming to be the one-stop destination for everyone who looking to participate with the financial markets, TradingLeagues will optimize stock trading activities and create an immersive experience for all users.</p>
+                  </div>
+                </div>
+              </div>
+              <div class="project-step__item row">
+                <div class="col-12 col-md-4 mb-3 mb-md-0">
+                  <span class="clr-second fnt-18 d-inline-block fnt-700 text-uppercase data-scroll disc-head text-rpd text-rpd text-rpd--more" style="opacity: 1; transform: translateY(0px);">Discover</span>
+                  </div>
+                  <div class="col-12 col-md-8 project-step-disc">
+                    <div class="project-step-disc__item">
+                    <p>Trading in the stock markets has been gaining popularity around the world, especially in India. Millennials and gen-z are turning to the stock markets to supplement their income from their daily jobs. This has been caused by growing awareness about the stock market and easy access to trading services at a low cost. With this, we began our research by conducting workshops and conversing with our stakeholders to understand the core consumers, their workflows, pain points, technical constraints, and their product goals and expectations. Based on what we defined the problem statement, we identified the approach to be taken for this project.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+  </div>
+</section>
+<!-- 3 -->
+<section>
+   <div class="container">
+    <div class="row">
+      <div class="col-12 col-md-11 col-lg-10 mx-auto">
+          <div class="d-flex">
+              <div class="col-12 col-md-4">
+                  <img class="img-fluid"  src= "<?php echo get_template_directory_uri(); ?>/img/trading-portfolio/Battle League.png">
+              </div>
+              <div class="col-12 col-md-4 mrg-btm-130">
+                  <img class="img-fluid"  src= "<?php echo get_template_directory_uri(); ?>/img/trading-portfolio/Battle League (2).png">
+              </div>
+              <div class="col-12 col-md-4">
+                  <img class="img-fluid"  src= "<?php echo get_template_directory_uri(); ?>/img/trading-portfolio/Battle League (1).png">
+              </div>
+          </div>  
+       </div>
+    </div>
+   </div>
+</section>
+<!-- 4 -->
+<section class="p-r-80 pb-0 pare-define">
+  <div class="container">
+    <div class="row">
+      <div class="col-12 col-md-11 col-lg-10 mx-auto">
+          <div class="col-md-10 px-0 mx-auto">
+          <div class="mb-r-80">
+            <div class="project-step">
+              <div class="project-step__item row">
+                <div class="col-12 col-md-4 mb-3 mb-md-0"> 
+                  <span class="clr-second fnt-18 d-inline-block fnt-700 text-uppercase data-scroll disc-head text-rpd text-rpd text-rpd--more" style="opacity: 1; transform: translateY(0px);">Define</span>
+                </div>
+                <div class="col-12 col-md-8 project-step-disc">
+                  <div class="project-step-disc__item">
+                    <p>India, a country with 1.38 Billion population, more than 90% of people don't understand fintech. With this app, we wanted to target users who are interested in trading but fear the platform due to its complexity. In order to define the platform, we worked closely with the TradingLeagues team. The mission was clear, to redefine how the average user interacts with the financial markets through the power of gamification, drive financial inclusion to the stock markets and help move the needle with investor participation rates. </p>
+                  </div>
+                </div>
+              </div>
+              <div class="project-step__item row">
+                <div class="col-12 col-md-4 mb-3 mb-md-0">
+                  <span class="clr-second fnt-18 d-inline-block fnt-700 text-uppercase data-scroll disc-head text-rpd text-rpd text-rpd--more" style="opacity: 1; transform: translateY(0px);">Wireframes</span>
+                  </div>
+                  <div class="col-12 col-md-8 project-step-disc">
+                    <div class="project-step-disc__item">
+                    <p>Our focus was on breaking the complexity of each game in an easy-to-understand manner. We segregated each game with its own landing pages, which allows users to go through one game at a time. Providing a ‘how to play’ page, was a big help for the first-time user. Through our wireframes, we provided the customers with in-depth information about the product, the games, rules, and policies which in turn built trust in the consumers’ minds.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+  </div>
+</section>
+<!-- 5 -->
+<section>
+   <div class="container">
+    <div class="row">
+            <div class="col-md-3 col-3">
+                <div class="iphonepro-con-1">
+                  <img class="img-fluid"  src= "<?php echo get_template_directory_uri(); ?>/img/trading-portfolio/iPhone 12 Pro.png">
+                  <img class="img-fluid"  src= "<?php echo get_template_directory_uri(); ?>/img/trading-portfolio/iPhone 12 Pro (1).png">
+                </div> 
+            </div>
+            <div class="col-md-3 col-3">
+                <div class="iphonepro-con-2">
+                  <img class="img-fluid"  src= "<?php echo get_template_directory_uri(); ?>/img/trading-portfolio/iPhone 12 Pro (2).png">
+                  <img class="img-fluid"  src= "<?php echo get_template_directory_uri(); ?>/img/trading-portfolio/iPhone 12 Pro (3).png">
+                </div>
+            </div>
+            <div class="col-md-3 col-3">
+                <div class="iphonepro-con-3">
+                  <img class="img-fluid"  src= "<?php echo get_template_directory_uri(); ?>/img/trading-portfolio/iPhone 12 Pro (4).png ">
+                  <img class="img-fluid"  src= "<?php echo get_template_directory_uri(); ?>/img/trading-portfolio/iPhone 12 Pro (5).png">
+                </div>
+            </div>
+            <div class="col-md-3 col-3">
+              <div class="iphonepro-con-4">
+                <img class="img-fluid"  src= "<?php echo get_template_directory_uri(); ?>/img/trading-portfolio/iPhone 12 Pro (6).png">
+                <img class="img-fluid"  src= "<?php echo get_template_directory_uri(); ?>/img/trading-portfolio/iPhone 12 Pro (7).png">
+              </div>
+            </div>
+             
+    </div>
+    
+   </div>
+</section>
+</main>
+ 
+<?php
+get_footer();
+?>
