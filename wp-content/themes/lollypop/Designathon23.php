1836d7c61e4236b9a05756285b2942403c364815
vandana <vandana.c@terralogic.com>
2023-04-12 12:20:30 +0530

    Update Designathon23.php

--- wp-content/themes/lollypop/Designathon23.php
+++ wp-content/themes/lollypop/Designathon23.php
@@ -635,7 +635,7 @@ p br{
       <section class="sec-pd-designathon2 pt-5 pb-5">
         <div class="container">
           <div class="row justify-content-center">
-              <h3 class="fnt-50 fnt-800 pb-md-4 clr-yel-92a text-center mb-5 our-sponser">Our Sponsor</h3>	  
+              <h3 class="fnt-50 fnt-800 pb-md-4 clr-yel-92a text-center mb-5 our-sponser">Our Sponsors</h3>	  
                     <div class="sponsor-img mb-5 mx-auto">
                       <div class="col-12 col-md-11 col-lg-9 col-xl-9 mx-auto d-flex sponsor-sec">
                           <div class="col-md-3">
