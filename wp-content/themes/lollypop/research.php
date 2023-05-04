a0ef52299d95be810ee9fc2036d9e0cefd8cbccc
Swatikumari49 <102963630+Swatikumari49@users.noreply.github.com>
2023-05-04 17:28:54 +0530

    Update research.php

--- wp-content/themes/lollypop/research.php
+++ wp-content/themes/lollypop/research.php
@@ -212,9 +212,9 @@ height:140px;
 								
             <div class="play-video-on-scroll">
               <div id="play2-out" style="display:none">
-                <div id="play2" data-plyr-provider="vimeo" data-plyr-embed-id="<?php echo get_field('researchbanner_video_link')?>"></div>
+                <div id="play2" data-plyr-provider="<?php echo get_field('researchbanner_video_provider')?>" data-plyr-embed-id="<?php echo get_field('researchbanner_video_link')?>"></div>
               </div>
-              <video class="playvid" autoplay="" muted="" loop="" playsinline="" provider=" vimeo" embed-id="<?php echo get_field('researchbanner_video_link')?>" poster="<?php echo get_field('researchbanner_image')?>">
+              <video class="playvid" autoplay="" muted="" loop="" playsinline="" provider=" <?php echo get_field('researchbanner_video_provider')?>" embed-id="<?php echo get_field('researchbanner_video_link')?>" poster="<?php echo get_field('researchbanner_image')?>">
               </video>
             </div>
 					<!--	<img class="img-fluid blog-d-img mb-0" src="<?php echo get_field('researchbanner_image')?>" alt="Image" width="1077" height="471" style="transform: translate(0px, 0px);">-->
