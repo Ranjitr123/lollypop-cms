68fae44d9ae29715d9a7171cd77c933897c8f4ec
Swatikumari49 <102963630+Swatikumari49@users.noreply.github.com>
2023-05-12 18:32:53 +0530

    Update salesforce-landing.php

--- wp-content/themes/lollypop/salesforce-landing.php
+++ wp-content/themes/lollypop/salesforce-landing.php
@@ -103,9 +103,9 @@ get_header();
 <section class="sec-pd sec-md-pt" id="sixth">
    <div class="container">
       <div class="row">
-         <div class="col-12 col-md-8 col-lg-8 offset-3 mx-auto">
+         <div class="col-12 col-md-8 col-lg-8 offset-2 mx-auto">
             <div class="mb-90 data-scroll" style="opacity: 1; transform: translateY(0px);">
-               <h2 class="fnt-50 fnt-800 mb-16 col-md-6 col-lg-12"><?php the_field('heading') ?></h2>
+               <h2 class="fnt-50 fnt-800 mb-16 col-md-12 col-lg-12"><?php the_field('heading') ?></h2>
                <p class="fnt-24 col-md-8 col-lg-8 px-0"><?php the_field('content') ?></p>
             </div>
          </div>
