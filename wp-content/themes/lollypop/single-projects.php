3aad988a7a20cdb0ff1c3e2f1c66ed5e1a7b2685
Swatikumari49 <102963630+Swatikumari49@users.noreply.github.com>
2023-06-09 12:08:41 +0530

    Update single-projects.php

--- wp-content/themes/lollypop/single-projects.php
+++ wp-content/themes/lollypop/single-projects.php
@@ -107,20 +107,7 @@ get_header();
 						</div>
 						<?php endwhile; endif; ?>
 					</div> 
-					<?php endif; ?>						
-
-					<?php if (empty(get_sub_field('images_section1')) === false): ?>
-						<div class="row">
-						<?php if( have_rows('images_section1') ) : while( have_rows('images_section1') ): the_row(); ?>
-						<div class="col-md-4">
-							<figure>
-								<img src="<?php the_sub_field('image1');?>" class="img-fluid w-100" alt="Image">
-							</figure>
-						</div>
-						<?php endwhile; endif; ?>
-					</div>  
-					<?php endif; ?>	
-						
+					<?php endif; ?>												
           
             <div class="photo position-relative"><!-----------mobile resposive------------->
 				<?php if( have_rows('images_section') ) : while( have_rows('images_section') ): the_row(); ?>
