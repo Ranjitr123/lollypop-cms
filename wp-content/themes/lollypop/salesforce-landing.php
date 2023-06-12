176b72c6b089942fbfe7fd2a11b35297dfb03f37
Swatikumari49 <102963630+Swatikumari49@users.noreply.github.com>
2023-06-12 16:42:52 +0530

    Update salesforce-landing.php

--- wp-content/themes/lollypop/salesforce-landing.php
+++ wp-content/themes/lollypop/salesforce-landing.php
@@ -271,7 +271,7 @@ get_header();
               <div class="py-70 px-3 data-scroll" style="background: #221429;">
                 <div class="d-flex align-items-center justify-content-center">
                   <div class="col-11 col-md-8 col-lg-8 px-0 d-inline-block clr-white text-center"> 
-                    <div class="mb-45"><span class="d-block fnt-24 mb-2 pb-2 mb-md-4 pb-md-0">Want impactful data insights?</span>
+                    <div class="mb-45"><span class="d-block fnt-24 mb-2 pb-2 mb-md-4 pb-md-0">Want to craft meaningful experiences?</span>
                       <h2 class="fnt-50 fnt-800 clr-white">Let's talk about your <br>Salesforce needs.</h2>
                     </div><a class="web-btn web-btn--red" href="<?php echo site_url(); ?>/project-enquiry/">GET IN TOUCH</a>
                   </div>
