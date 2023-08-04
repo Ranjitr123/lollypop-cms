4024ea594fe35d4b362cfc650d291200f2cd6d7e
Hai Tran <hai.tran@terralogic.com>
2023-08-04 16:00:40 +0700

    Speed for word career changed

--- wp-content/themes/lollypop/js/career.js
+++ wp-content/themes/lollypop/js/career.js
@@ -1,1 +1,44 @@
-!function n(i,u,o){function f(t,e){if(!u[t]){if(!i[t]){var r="function"==typeof require&&require;if(!e&&r)return r(t,!0);if(a)return a(t,!0);throw new Error("Cannot find module '"+t+"'")}e=u[t]={exports:{}};i[t][0].call(e.exports,function(e){var r=i[t][1][e];return f(r||e)},e,e.exports,n,i,u,o)}return u[t].exports}for(var a="function"==typeof require&&require,e=0;e<o.length;e++)f(o[e]);return f}({1:[function(require,e,exports){"use strict";$(document).ready(function(){$("header").addClass("lp-mainheader--white");$(function(){var e=["artists","architects","writers","engineers"],r=0;setInterval(function(){$(".quote-career").fadeOut(function(){$(this).html(e[r=(r+1)%e.length]).fadeIn()})},2500)});r=$(".quotes"),t=-1,function e(){++t;r.eq(t%r.length).fadeIn(1e3).delay(1e3).fadeOut(1e3,e)}();var r,t})},{}]},{},[1]);
+! function n(i, u, o) {
+    function f(t, e) {
+        if (!u[t]) {
+            if (!i[t]) {
+                var r = "function" == typeof require && require;
+                if (!e && r) return r(t, !0);
+                if (a) return a(t, !0);
+                throw new Error("Cannot find module '" + t + "'")
+            }
+            e = u[t] = {
+                exports: {}
+            };
+            i[t][0].call(e.exports, function(e) {
+                var r = i[t][1][e];
+                return f(r || e)
+            }, e, e.exports, n, i, u, o)
+        }
+        return u[t].exports
+    }
+    for (var a = "function" == typeof require && require, e = 0; e < o.length; e++) f(o[e]);
+    return f
+}({
+    1: [function(require, e, exports) {
+        "use strict";
+        $(document).ready(function() {
+            $("header").addClass("lp-mainheader--white");
+            $(function() {
+                var e = ["artists", "architects", "writers", "engineers"],
+                    r = 0;
+                setInterval(function() {
+                    $(".quote-career").fadeOut(function() {
+                        $(this).html(e[r = (r + 1) % e.length]).fadeIn()
+                    })
+                }, 2500)
+            });
+            r = $(".quotes"), t = -1,
+                function e() {
+                    ++t;
+                    r.eq(t % r.length).fadeIn(500).delay(500).fadeOut(500, e)
+                }();
+            var r, t
+        })
+    }, {}]
+}, {}, [1]);
