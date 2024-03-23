f254d017b8eb44b554796ee03dc8f253984c9158
man.nguyen <man.nguyen@terralogic.com>
2024-03-23 09:12:36 +0700

    feat: conlusion, dev section

--- wp-content/themes/lollypop/project-accreda.php
+++ wp-content/themes/lollypop/project-accreda.php
@@ -87,6 +87,102 @@ get_header();
   .mb-r-12 {
     padding-bottom: 12px;
   }
+
+  .wf-wrapper {
+    position: relative;
+  }
+
+  .blur-white {
+    position: absolute;
+    height: 235px;
+    bottom: -1px;
+    width: 100%;
+    background: url("<?php echo get_template_directory_uri(); ?>/assets/images/accreda/white-blur.png");
+    z-index: 1;
+  }
+
+  /* typography */
+  .mt-120 {
+    margin-top: 120px;
+  }
+
+  .typography-bg {
+    background: #5C1A53;
+    padding: 80px 94px;
+  }
+
+  .clr-typo {
+    color: #BA96B5;
+  }
+
+  .typo-rule {
+    border: 1px dashed #DAA75B;
+    margin-top: 72px;
+    margin-bottom: 40px;
+  }
+
+  /* Colour */
+
+  .clr-lightblack {
+    color: #221429;
+  }
+
+  .mt-48 {
+    margin-top: 48px;
+  }
+
+  .item-img:nth-child(2) {
+    width: 100px;
+  }
+
+  .mt-30 {
+    margin-top: 30px;
+  }
+
+  /* development */
+  .dev-bg {
+    background: url("<?php echo get_template_directory_uri(); ?>/assets/images/accreda/dev-top.svg"),
+      url("<?php echo get_template_directory_uri(); ?>/assets/images/accreda/dev-bottom.svg");
+    background-position: right top, left bottom;
+    background-repeat: no-repeat, no-repeat;
+    background-color: #fcf8f2;
+    padding: 50px 54px 42px 50px;
+  }
+
+  .clr-purple {
+    color: #5C1A53;
+  }
+
+  .fnt-800 {}
+
+  .dev-title {
+    font-family: Playfair Display;
+    color: #CD9E55;
+    margin-top: 24px;
+  }
+
+  .dev-heading {
+    color: #5C1A53;
+    font-weight: 800;
+    margin-top: 4px;
+    margin-bottom: 8px;
+  }
+
+  .dev-content {
+    color: #221429;
+    font-size: 14px;
+    line-height: 22px;
+  }
+
+  .mt-32 {
+    margin-top: 32px;
+  }
+
+  @media(max-width:768px) {
+    .typography-bg {
+      padding: 32px;
+    }
+  }
 </style>
 
 
@@ -142,7 +238,7 @@ get_header();
                 <?php if (have_rows('brief_descriptions')) : while (have_rows('brief_descriptions')) : the_row(); ?>
                     <div class="project-step__item row">
                       <div class="col-12 col-md-4 mb-3 mb-md-0">
-                        <span class="clr-second fnt-18 d-inline-block fnt-700 text-uppercase data-scroll disc-head text-rpd text-rpd text-rpd--more"><?php the_sub_field('title'); ?></span>
+                        <span class="clr-second fnt-18 d-inline-block fnt-700 text-uppercase data-scroll disc-head text-rpd text-rpd--more"><?php the_sub_field('title'); ?></span>
                       </div>
                       <div class="col-12 col-md-8 project-step-disc">
                         <div class="project-step-disc__item">
@@ -272,9 +368,9 @@ get_header();
   <section class="sec-pd sec-bnr p-r-80">
     <div class="container">
       <div class="row">
-        <div class="col-12 col-md-11 col-lg-10 mx-auto">
+        <div class="col-12 col-md-11 col-lg-11 mx-auto">
           <div class="row justify-content-center">
-            <div class="col-6 col-md-5">
+            <div class="col-12 col-md-5 wf-wrapper">
               <div class="mb-r-12 col-12 col-md-12 col-lg-12">
                 <span class="mb-2 mt-5 clr-second d-inline-block fnt-22 fnt-500 text-uppercase  data-scroll disc-head">
                   Wireframes for Web
@@ -290,23 +386,50 @@ get_header();
               </div>
               <div class="col-12 col-md-12 col-lg-12">
                 <?php if (have_rows('left_images')) : while (have_rows('left_images')) : the_row(); ?>
-                    <img class="mt-3" src="<?php the_sub_field('image'); ?>" alt="logo" />
+                    <img class="img-fluid mt-3" src="<?php the_sub_field('image'); ?>" alt="logo" />
                 <?php endwhile;
                 endif; ?>
               </div>
-              <div class="blur-white col-12 col-md-12 col-lg-12">
-                blur-white
-              </div>
+              <div class="blur-white col-12 col-md-12 col-lg-12"></div>
             </div>
-            <div class="col-6 col-md-5">
+            <div class="col-12 col-md-5 wf-wrapper">
               <div class="col-12 col-md-12 col-lg-12">
                 <?php if (have_rows('right_images')) : while (have_rows('right_images')) : the_row(); ?>
-                    <img class="mt-3" src="<?php the_sub_field('image'); ?>" alt="logo" />
+                    <img class="img-fluid mt-3" src="<?php the_sub_field('image'); ?>" alt="logo" />
                 <?php endwhile;
                 endif; ?>
               </div>
-              <div class="blur-white col-12 col-md-12 col-lg-12">
-                blur-white
+              <div class="blur-white col-12 col-md-12 col-lg-12"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- Typography -->
+  <section class="sec-typo mt-120">
+    <div class="container">
+      <div class="row">
+        <div class="col-12 col-md-11 col-lg-10 mx-auto">
+          <div class="row d-flex justify-content-center">
+            <div class="col-11 col-lg-10 typography-bg ms-lg-3 ms-0">
+              <div class="row">
+                <div class="col-12 col-md-5 px-0">
+                  <img src="<?php echo get_template_directory_uri(); ?>/assets/images/accreda/typography-vector.svg" alt="typo" />
+                  <h3 class="clr-white mt-5"><?php the_field('typo_title'); ?></h3>
+                </div>
+                <div class="col-12 col-md-7 px-0">
+                  <p class="clr-typo text-end fnt-400"><?php the_field('typo_content'); ?></p>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-12 px-0 typo-rule">
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-10 px-0">
+                  <img class="img-fluid" src="<?php the_field('typo_img'); ?>" alt="typo" />
+                </div>
               </div>
             </div>
           </div>
@@ -315,6 +438,189 @@ get_header();
     </div>
   </section>
 
+  <!-- Colors -->
+  <section class="sec-colo mt-120">
+    <div class="container">
+      <div class="row">
+        <div class="col-12 col-md-11 col-lg-10 mx-auto">
+          <div class="row d-flex justify-content-center">
+            <div class="col-6">
+              <div class="row">
+                <div class="col-12 col-md-12 px-0">
+                  <h3 class="clr-second text-uppercase text-center fnt-18 fnt-700"><?php the_field('color_title'); ?></h3>
+                </div>
+                <div class="col-12 col-md-12 px-0">
+                  <p class="clr-lightblack text-center fnt-400"><?php the_field('color_content'); ?></p>
+                </div>
+              </div>
+              <div class="row mt-48">
+                <div class="col-12">
+                  <img class="img-fluid" src="<?php the_field('color_img'); ?>" alt="typo" />
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Web UI -->
+  <section class="sec-web-ui mt-120">
+    <div class="container">
+      <div class="row">
+        <div class="col-12 col-md-11 col-lg-10 mx-auto">
+          <div class="row d-flex justify-content-center">
+            <div class="col-10 ">
+              <div class="row d-flex justify-content-center">
+                <div class="col-12 col-md-12 px-0">
+                  <h3 class="clr-second text-uppercase text-center fnt-18 fnt-700"><?php the_field('ui_title'); ?></h3>
+                </div>
+                <div class="col-12 col-md-7 px-0">
+                  <p class="clr-lightblack text-center fnt-400"><?php the_field('ui_content'); ?></p>
+                </div>
+              </div>
+
+              <div class="row mt-48">
+                <div class="col-12 ui-items">
+                  <?php if (have_rows('ui_images')) : while (have_rows('ui_images')) : the_row(); ?>
+                      <img class="img-fluid" src="<?php the_sub_field('image_1'); ?>" alt="typo" />
+                      <img class="img-fluid" src="<?php the_sub_field('image_2'); ?>" alt="typo" />
+                      <img class="img-fluid mt-30" src="<?php the_sub_field('image_3'); ?>" alt="typo" />
+                      <img class="img-fluid mt-120" src="<?php the_sub_field('image_4'); ?>" alt="typo" />
+                      <img class="img-fluid mt-120" src="<?php the_sub_field('image_5'); ?>" alt="typo" />
+                  <?php endwhile;
+                  endif; ?>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Development Process -->
+  <section class="sec-development mt-120">
+    <div class="container">
+      <div class="row">
+        <div class="col-12 col-md-11 col-lg-10 mx-auto">
+          <div class="row d-flex justify-content-center">
+            <div class="col-10 ">
+              <div class="row d-flex justify-content-center">
+                <div class="col-12 col-md-12 px-0">
+                  <h3 class="clr-second text-uppercase text-center fnt-18 fnt-700"><?php the_field('dev_title'); ?></h3>
+                </div>
+              </div>
+              <div class="row mt-32">
+                <div class="col-12 dev-bg">
+                  <div class="row mt-2 mt-lg-4">
+                    <div class="col-6">
+                      <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/images/accreda/dev-vector.svg" alt="" />
+                      <h3 class="dev-title">Development Process:</h3>
+                    </div>
+                  </div>
+                  
+                  <div class="row mt-2 mt-lg-4">
+                    <div class="col-5">
+                      <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/images/accreda/dev-bullet.svg" alt="" />
+                      <p class="dev-heading">Agile Methodology</p>
+                      <p class="dev-content">Accreda embraces Agile development for iterative, adaptive processes, ensuring quick adaptation to changing needs and enhancing search visibility.</p>
+                    </div>
+                    <div class="col-5 pe-0">
+                      <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/images/accreda/dev-bullet.svg" alt="" />
+                      <p class="dev-heading">Testing & Quality Assurance</p>
+                      <p class="dev-content">Thorough testing occurs at every development stage to guarantee the platform's reliability, security, and performance. This approach aims to streamline</p>
+                    </div>
+                  </div>
+
+                  <div class="row">
+                    <div class="col-6 mt-3 mt-lg-4">
+                      <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/images/accreda/dev-vector.svg" alt="" />
+                      <h3 class="dev-title">Challenges and Solutions:</h3>
+                    </div>
+                  </div>
+                  <div class="row mt-2 mt-lg-4">
+                    <div class="col-4">
+                      <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/images/accreda/dev-bullet.svg" alt="" />
+                      <p class="dev-heading">Scalability</p>
+                      <p class="dev-content">To ensure scalability, microservices architecture implemented, load-balancing techniques were employed.</p>
+                    </div>
+                    <div class="col-4">
+                      <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/images/accreda/dev-bullet.svg" alt="" />
+                      <p class="dev-heading">Performance Optimisation:</p>
+                      <p class="dev-content">Caching mechanisms were used to optimise the platform's performance to optimisation </p>
+                    </div>
+                    <div class="col-4">
+                      <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/images/accreda/dev-bullet.svg" alt="" />
+                      <p class="dev-heading">Security</p>
+                      <p class="dev-content">Best practices for web application security implemented, including encryption, secure coding practices, regular security audits.</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- Architecture -->
+  <section class="sec-architecture mt-120">
+    <div class="container">
+      <div class="row">
+        <div class="col-12 col-md-11 col-lg-10 mx-auto">
+          <div class="row d-flex justify-content-center">
+            <div class="col-10 ">
+              <div class="row d-flex justify-content-center">
+                <div class="col-12 col-md-12 px-0">
+                  <h3 class="clr-second text-uppercase text-center fnt-18 fnt-700"><?php the_field('architecture_title'); ?></h3>
+                </div>
+              </div>
+              <div class="row mt-48">
+                <div class="col-12 ui-items">
+                  <img class="img-fluid" src="<?php the_field('architecture_img'); ?>" alt="typo" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Comlcusion -->
+  <?php if (get_field('conclusion') != '') {  ?>
+    <section class="conclusion-pad sec-pt">
+      <div class="container">
+        <div class="row">
+          <div class="col-12 col-md-11 col-lg-12 mt-5">
+            <div class="col-md-10 px-0 mx-auto">
+              <div>
+                <div class="col-md-10 px-0 mx-auto">
+                  <div class="mb-r-80">
+                    <div class="project-step">
+                      <div class="project-step__item row">
+                        <div class="col-12 mb-4">
+                          <h3 class="clr-second fnt-40 text-left fnt-800 d-inline-block data-scroll">Conclusion</h3>
+                        </div>
+                        <div class="col-12 col-md-10 project-step-disc">
+                          <div class="project-step-disc__item conclustion">
+                            <?php the_field('conclusion'); ?>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  <?php  } ?>
   <!--05-->
   <section class="sec-footer-trans">
     <div class="container z-1">
